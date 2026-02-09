821a403c303bf77e5c08d2ddd91988397c025509
Zhou Hongyu <zhouhongyucs@163.com>
2026-02-09 13:37:41 +0800

    Hello World

--- chap1/task1.cpp
+++ chap1/task1.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 
 int main() {
-    std::cout<<"姓名：张三"<<std::endl;
-    std::cout<<"年龄：18"<<std::endl;
-    std::cout<<"学校：清华大学计算机系"<<std::endl;
+    std::cout<<"Hello World!"<<std::endl;
     return 0;
 }
